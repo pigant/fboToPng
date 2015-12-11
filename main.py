ad46ba746220f69d660d553e24ae2e936f0b678b
Ignacio Ruz <pigant666@hotmail.com>
2015-12-11 13:40:30 -0300

    Se captura la camara web, se persiste la foto

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import kivy
 from kivy.app import App
 from kivy.graphics import Fbo
 from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.graphics import Rectangle, Color
+from kivy.graphics.texture import Texture
+from kivy.vector import Vector
 
 
 class Tabla(Widget):
@@ -30,32 +33,63 @@ class Tabla(Widget):
         self.f.texture.mag_filter = 'nearest'
 
     def llorar(self):
+        global write_png
         print('El boton es: %s' % (self.button))
         print('el array es: %s' % (len(self.f.pixels)))
-        b = self.write_png(self.f.pixels, 4, 4)
+        b = write_png(self.f.pixels, 4, 4)
         print(b)
         f = open('a.png', 'w')
         f.write(b)
         
-    def write_png(self, buf, width, height):
-        import zlib, struct
-        width_byte_4 = width * 4
-        raw_data = b"".join(\
-            b'\x00' + buf[span:span + width_byte_4] for span \
-            in range((height - 1) * width * 4, -1, - width_byte_4))
-        def png_pack(png_tag, data):
-            chunk_head = png_tag + data
-            return struct.pack("!I", len(data)) + chunk_head + struct.pack("!I", 0xFFFFFFFF & zlib.crc32(chunk_head))
-        return b"".join([
-            b'\x89PNG\r\n\x1a\n',
-            png_pack(b'IHDR', struct.pack("!2I5B", width, height, 8, 6, 0, 0, 0)),
-            png_pack(b'IDAT', zlib.compress(raw_data, 9)),
-            png_pack(b'IEND', b'')])
+def write_png(buf, width, height):
+    import zlib, struct
+    width_byte_4 = width * 4
+    raw_data = b"".join(\
+        b'\x00' + buf[span:span + width_byte_4] for span \
+        in range((height - 1) * width * 4, -1, - width_byte_4))
+    def png_pack(png_tag, data):
+        chunk_head = png_tag + data
+        return struct.pack("!I", len(data)) + chunk_head + \
+            struct.pack("!I", 0xFFFFFFFF & zlib.crc32(chunk_head))
+    return b"".join([
+        b'\x89PNG\r\n\x1a\n',
+        png_pack(b'IHDR', struct.pack("!2I5B", width, height, 8, 6, 0, 0, 0)),
+        png_pack(b'IDAT', zlib.compress(raw_data, 9)),
+        png_pack(b'IEND', b'')])
+
+class Camara(FloatLayout):
+    camera = ObjectProperty()
+
+    def mostrarCamara(self):
+        global write_png
+        #print self.camera.texture.pixels
+        data = self.camera.texture.pixels
+        #invierte los pixeles
+        l = data
+        c = b''.join(l[-4:])
+        for a in (l[-4*(x+2):-4*(x+1)] for x in range((len(l)-4)/4) ):
+              c = c + a
+        data = c
+        #tex = Texture.create(size=(1280,720))
+        #tex.blit_buffer(data, colorfmt='rgba', bufferfmt='ubyte')
+        print('pos: %s size: %s' % (self.pos, self.size))
+        with self.canvas:
+            self.fbo = Fbo(size=self.camera.resolution)
+            Rectangle( \
+                size=Vector(self.size) * 0.5, \
+                pos= self.pos, \
+                texture=self.fbo.texture)
+        with self.fbo:
+            Rectangle(size=self.fbo.size, texture=self.camera.texture)
+        png = write_png(data, 1280, 720)
+        f = open('foto.png', 'w')
+        f.write(png)
 
 
 class VentanaApp(App):
     def __init(self, **kwargs):
         super(VentanaApp, self).__init__(**kwargs)
+        self.camera = self.root.ids.camera
 
 
 if __name__ == '__main__':
