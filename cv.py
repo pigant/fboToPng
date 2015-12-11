7df52cd7877489a72e1e0f70d75f7b25e24abf26
Ignacio Ruz <pigant666@hotmail.com>
2015-12-11 15:48:15 -0300

    Intento de opencv

---
+++ cv.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fourcc = cv2.cv.CV_FOURCC(*'XDIV')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+#while(cap.isOpened()):
+ret, frame = cap.read()
+#if ret==True:
+frame = cv2.flip(frame,0)
+
+# write the flipped frame
+out.write(frame)
+
+#cv2.imshow('frame',frame)
+#if cv2.waitKey(1) & 0xFF == ord('q'):
+    #break
+#else:
+#break
+
+c = b''
+for ll in frame:
+    for l in ll:
+        c = c.join(str(x) for x in l.tolist() + [255])
+print c
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
